9d4011bc5447f48d3da303d73cfe9420cad0b9d6
Ashwani singh <43879028+Ashwani66@users.noreply.github.com>
2019-04-10 19:52:17 +0530

    Extracting Data from JSON
    Week 6 assignment Solution

---
+++ Week 6 (1).py
@@ -0,0 +1,31 @@
+import urllib.request as ur
+import json
+
+# json_url = 'http://python-data.dr-chuck.net/comments_42.json'
+
+json_url = input("Enter location: ")
+print("Retrieving ", json_url)
+data = ur.urlopen(json_url).read().decode('utf-8')
+print('Retrieved', len(data), 'characters')
+json_obj = json.loads(data)
+
+sum = 0
+total_number = 0
+
+for comment in json_obj["comments"]:
+    sum += int(comment["count"])
+    total_number += 1
+
+print('Count:', total_number)
+print('Sum:', sum)
+
+
+"""
+Output:
+Enter location:   http://py4e-data.dr-chuck.net/comments_187846.json
+
+Retrieving   http://py4e-data.dr-chuck.net/comments_187846.json
+Retrieved 2726 characters
+Count: 50
+Sum: 2756
+"""
